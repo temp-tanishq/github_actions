9fa2067b075d8eae54a0ea67ae90a9e7eeb7b5ad
Tanishq123467658 <battultanishq.com>
2026-07-14 16:40:43 +0530

    Fix incorrect expected result in test for add_numbers function

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_app_number():
     assert add_numbers(2, 3) == 5
     assert add_numbers(0, 0) == 0
     assert add_numbers(-1, 1) == 0
-    assert add_numbers(11, 11) == 1111
+    assert add_numbers(11, 11) == 22
